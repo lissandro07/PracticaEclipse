78fe313a561e09a00c925c3da1f37abb57a28c1d
lissandro07 <cm22118@ues.edu.sv>
2024-05-04 12:53:43 -0600

    mi primer commit

---
+++ demo-cm22118/src/main/java/com/iis/cm22118/App.java
@@ -0,0 +1,18 @@
+package com.iis.cm22118;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public String Hello() {
+    	return "Hello World";
+    }
+    
+	public static void main( String[] args )
+    {
+        App hw = new App();
+		System.out.println(hw.Hello());
+    }
+}
